c7788363d0734dfaeff3ddc2ffddde495b09d56d
Technialirfan <iu507713@gmail.com>
2026-06-23 01:09:10 +0500

    Remove hardcoded bypass so new account works

--- blrs-backend/config/db.js
+++ blrs-backend/config/db.js
@@ -3,14 +3,7 @@ global.mongoError = null;
 
 const connectDB = async () => {
   try {
-    let uri = process.env.MONGODB_URI;
-    // VERCEL SRV BUG BYPASS FOR NEW CLUSTER:
-    if (uri && uri.startsWith("mongodb+srv://")) {
-      const credentials = uri.split("@")[0].replace("mongodb+srv://", "");
-      uri = `mongodb://${credentials}@ac-0t7pmgf-shard-00-00.sm4jyvs.mongodb.net:27017,ac-0t7pmgf-shard-00-01.sm4jyvs.mongodb.net:27017,ac-0t7pmgf-shard-00-02.sm4jyvs.mongodb.net:27017/blrs_db?ssl=true&replicaSet=atlas-obii6c-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    }
-
-    const conn = await mongoose.connect(uri, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
       family: 4
