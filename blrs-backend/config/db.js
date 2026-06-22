d75cb9bbad8979769ac61a1d599f7723aa2f0ff0
Technialirfan <iu507713@gmail.com>
2026-06-22 23:48:34 +0500

    Fix Replica Set name in Vercel SRV bypass

--- blrs-backend/config/db.js
+++ blrs-backend/config/db.js
@@ -3,7 +3,14 @@ global.mongoError = null;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    let uri = process.env.MONGODB_URI;
+    // VERCEL SRV BUG BYPASS:
+    if (uri && uri.startsWith("mongodb+srv://")) {
+      const credentials = uri.split("@")[0].replace("mongodb+srv://", "");
+      uri = `mongodb://${credentials}@ac-gzs7wsl-shard-00-00.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-01.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-02.y5zkmio.mongodb.net:27017/blrs_db?ssl=true&replicaSet=atlas-g1s9ee-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    }
+
+    const conn = await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
     });
