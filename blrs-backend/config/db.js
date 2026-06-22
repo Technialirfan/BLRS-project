26dbf0fa153ca73653a0fb7eedc734d8b8724e99
Technialirfan <iu507713@gmail.com>
2026-06-22 23:50:58 +0500

    Fix Vercel DNS hang natively with family:4

--- blrs-backend/config/db.js
+++ blrs-backend/config/db.js
@@ -3,16 +3,10 @@ global.mongoError = null;
 
 const connectDB = async () => {
   try {
-    let uri = process.env.MONGODB_URI;
-    // VERCEL SRV BUG BYPASS:
-    if (uri && uri.startsWith("mongodb+srv://")) {
-      const credentials = uri.split("@")[0].replace("mongodb+srv://", "");
-      uri = `mongodb://${credentials}@ac-gzs7wsl-shard-00-00.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-01.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-02.y5zkmio.mongodb.net:27017/blrs_db?ssl=true&replicaSet=atlas-g1s9ee-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    }
-
-    const conn = await mongoose.connect(uri, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
+      family: 4 // Force IPv4 to fix Vercel Node 18 DNS hang
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
