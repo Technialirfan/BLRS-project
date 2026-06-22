c4f8edf860ca3a6452f4b1ac95c3e5629b8c4b6f
Technialirfan <iu507713@gmail.com>
2026-06-22 23:52:08 +0500

    Deploy the CORRECT Vercel SRV bypass

--- blrs-backend/config/db.js
+++ blrs-backend/config/db.js
@@ -3,10 +3,16 @@ global.mongoError = null;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    let uri = process.env.MONGODB_URI;
+    // VERCEL SRV BUG BYPASS:
+    if (uri && uri.startsWith("mongodb+srv://")) {
+      const credentials = uri.split("@")[0].replace("mongodb+srv://", "");
+      uri = `mongodb://${credentials}@ac-gzs7wsl-shard-00-00.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-01.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-02.y5zkmio.mongodb.net:27017/blrs_db?ssl=true&replicaSet=atlas-g1s9ee-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    }
+
+    const conn = await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
-      family: 4 // Force IPv4 to fix Vercel Node 18 DNS hang
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
