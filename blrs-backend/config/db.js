9190708a50ff29475398647fbb9ee9cad9f1138f
Technialirfan <iu507713@gmail.com>
2026-06-22 23:46:06 +0500

    Restore standard mongoose connection

--- blrs-backend/config/db.js
+++ blrs-backend/config/db.js
@@ -3,14 +3,7 @@ global.mongoError = null;
 
 const connectDB = async () => {
   try {
-    let uri = process.env.MONGODB_URI;
-    // VERCEL SRV BUG BYPASS:
-    if (uri && uri.startsWith("mongodb+srv://")) {
-      const credentials = uri.split("@")[0].replace("mongodb+srv://", "");
-      uri = `mongodb://${credentials}@ac-gzs7wsl-shard-00-00.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-01.y5zkmio.mongodb.net:27017,ac-gzs7wsl-shard-00-02.y5zkmio.mongodb.net:27017/blrs_db?ssl=true&replicaSet=atlas-gzs7wsl-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    }
-    
-    const conn = await mongoose.connect(uri, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
     });
